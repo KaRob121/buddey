121b9d65af744284b88244030faabd42bdec1dea
Karl Robin <24803299+KaRob121@users.noreply.github.com>
2022-05-10 21:21:49 -0700

    Added key to each bill in bill list; created a functioning delete bill button for each bill; fixed bug regarding multiple bill lists being created after adding a new bill

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -10,9 +10,10 @@ function Bills() {
 
   const [allBills, setAllBills] = React.useState([])
 
-  const billsList = allBills.map(bill => {
+  const billsList = allBills.map((bill, ind) => {
     return (
-      <div className="Bills--list">
+      <div className="Bills--bill" key={ ind } >
+        <button onClick={ () => removeBill(ind) }>x</button>
         <h1>{ bill.name }</h1>
         <h1>{ bill.amountDue}</h1>
         <h1>{ bill.dueDate }</h1>
@@ -20,12 +21,22 @@ function Bills() {
     )
   })
 
-  function addBill(event) {
+  function addBill() {
     setAllBills(prevAllBills => {
       return [
         bill,
         ...prevAllBills
       ]
+    })
+
+
+  }
+
+  function removeBill(ind) {
+    setAllBills(prevAllBills => {
+      const newList = [...prevAllBills]
+      newList.splice(ind, 1)
+      return newList
     })
   }
 
@@ -63,8 +74,10 @@ function Bills() {
         value={ bill.dueDate }
         onChange={ handleChange }
       />
-      <button type="reset" onClick={ addBill }>Add a new bill</button>
-      { billsList }
+      <button type="submit" onClick={ addBill }>Add a new bill</button>
+      <div className="Bills-list">
+        { billsList }
+      </div>
     </div>
   )
 }
